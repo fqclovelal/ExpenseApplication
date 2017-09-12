35cdc00e3c68ea64949393006930767601a47e0b
PC-201707140940\Administrator <fqclovelal@qq.com>
2017-09-12 16:36:36 +0800

    Initial commit

---
+++ app/src/main/java/com/expenseapplication/AsyncHeaderViewHolder.java
@@ -0,0 +1,34 @@
+package com.expenseapplication;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by ericliu on 12/10/16.
+ */
+
+public abstract class AsyncHeaderViewHolder extends RecyclerView.ViewHolder implements AsyncExpandableListView.OnGroupStateChangeListener {
+    private final int mGroupOrdinal;
+    private final AsyncExpandableListView mAsyncExpandableListView;
+
+    public AsyncHeaderViewHolder(View itemView, int groupOrdinal, AsyncExpandableListView asyncExpandableListView) {
+        super(itemView);
+        mGroupOrdinal = groupOrdinal;
+        mAsyncExpandableListView = asyncExpandableListView;
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAsyncExpandableListView.onGroupClicked(mGroupOrdinal);
+                onItemClick(v);
+            }
+        });
+    }
+
+    /**
+     * triggered by onClick event, to be overriden.
+     * @param view
+     */
+    public void onItemClick(View view){}
+
+
+}
---
+++ app/src/main/java/com/expenseapplication/MainActivity.java
@@ -0,0 +1,217 @@
+package com.expenseapplication;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AsyncExpandableListViewCallbacks<String, News> {
+    private static AsyncExpandableListView<String, News> mAsyncExpandableListView;
+    private CollectionView.Inventory<String, News> inventory;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAsyncExpandableListView = (AsyncExpandableListView) findViewById(R.id.asyncExpandableCollectionView);
+        mAsyncExpandableListView.setCallbacks(this);
+        inventory = new CollectionView.Inventory<>();
+        CollectionView.InventoryGroup<String, News> group1 = inventory.newGroup(0); // groupOrdinal is the smallest, displayed first
+        group1.setHeaderItem("Top Stories");
+
+
+        CollectionView.InventoryGroup<String, News> group2 = inventory.newGroup(2);
+        group2.setHeaderItem("World");
+
+
+        CollectionView.InventoryGroup<String, News> group3 = inventory.newGroup(3);
+        group3.setHeaderItem("Australia");
+
+        CollectionView.InventoryGroup<String, News> group4 = inventory.newGroup(4);
+        group4.setHeaderItem("International");
+
+        CollectionView.InventoryGroup<String, News> group5 = inventory.newGroup(5);
+        group5.setHeaderItem("Businesses");
+
+        CollectionView.InventoryGroup<String, News> group6 = inventory.newGroup(6);
+        group6.setHeaderItem("Technology");
+
+        CollectionView.InventoryGroup<String, News> group7 = inventory.newGroup(7);
+        group7.setHeaderItem("Environment");
+
+        CollectionView.InventoryGroup<String, News> group8 = inventory.newGroup(8);
+        group8.setHeaderItem("Health");
+
+        CollectionView.InventoryGroup<String, News> group9 = inventory.newGroup(9);
+        group9.setHeaderItem("Science");
+
+        CollectionView.InventoryGroup<String, News> group10 = inventory.newGroup(10);
+        group10.setHeaderItem("Sports");
+
+        CollectionView.InventoryGroup<String, News> group11 = inventory.newGroup(11);
+        group11.setHeaderItem("Entertainment");
+
+        CollectionView.InventoryGroup<String, News> group12 = inventory.newGroup(12);
+        group12.setHeaderItem("Politics");
+
+        mAsyncExpandableListView.updateInventory(inventory);
+
+    }
+
+    @Override
+    public void onStartLoadingGroup(int groupOrdinal) {
+        new LoadDataTask(groupOrdinal, mAsyncExpandableListView).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+    }
+
+    private static class LoadDataTask extends AsyncTask<Void, Void, Void> {
+
+        private final int mGroupOrdinal;
+        private WeakReference<AsyncExpandableListView<String, News>> listviewRef = null;
+
+        public LoadDataTask(int groupOrdinal, AsyncExpandableListView<String, News> listview) {
+            mGroupOrdinal = groupOrdinal;
+            listviewRef = new WeakReference<>(listview);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            List<News> items = new ArrayList<>();
+            News news = new News();
+            news.setNewsTitle("Lawyers meet voluntary pro bono target for first time since 2013");
+            news.setNewsBody("A voluntary target for the amount of pro bono work done by Australian lawyers has been met for the first time since 2013. Key points: The Australian Pro Bono Centre's asks lawyers to do 35 hours of free community work a year; Pro bono services can help ...\n");
+            items.add(news);
+
+            news = new News();
+            news.setNewsTitle("HSC 2016: 77000 students to sit first exams across NSW");
+            news.setNewsBody("More than 77,000 NSW high school students will sit their first HSC exams this week as one of the final cohorts to sit the test before the NSW government enacts sweeping reforms across the state.");
+            items.add(news);
+
+            if (listviewRef.get() != null) {
+                listviewRef.get().onFinishLoadingGroup(mGroupOrdinal, items);
+                mAsyncExpandableListView.smoothScrollToPosition(mGroupOrdinal);
+              
+            }
+        }
+
+    }
+
+    @Override
+    public AsyncHeaderViewHolder newCollectionHeaderView(Context context, int groupOrdinal, ViewGroup parent) {
+        View v = LayoutInflater.from(context)
+                .inflate(R.layout.header_row_item_async, parent, false);
+
+        return new MyHeaderViewHolder(v, groupOrdinal, mAsyncExpandableListView);
+    }
+
+    @Override
+    public RecyclerView.ViewHolder newCollectionItemView(Context context, int groupOrdinal, ViewGroup parent) {
+        View v = LayoutInflater.from(context)
+                .inflate(R.layout.text_row_item_async, parent, false);
+
+        return new NewsItemHolder(v);
+    }
+
+    @Override
+    public void bindCollectionHeaderView(Context context, AsyncHeaderViewHolder holder, int groupOrdinal, String headerItem) {
+        MyHeaderViewHolder myHeaderViewHolder = (MyHeaderViewHolder) holder;
+        myHeaderViewHolder.getTextView().setText(headerItem);
+    }
+
+    @Override
+    public void bindCollectionItemView(Context context, RecyclerView.ViewHolder holder, int groupOrdinal, News item) {
+        NewsItemHolder newsItemHolder = (MainActivity.NewsItemHolder) holder;
+        newsItemHolder.getTextViewTitle().setText(item.getNewsTitle());
+        newsItemHolder.getTextViewDescrption().setText(item.getNewsBody());
+    }
+    public static class MyHeaderViewHolder extends AsyncHeaderViewHolder implements AsyncExpandableListView.OnGroupStateChangeListener {
+
+        private final TextView textView;
+        private final ProgressBar mProgressBar;
+        private ImageView ivExpansionIndicator;
+
+        public MyHeaderViewHolder(View v, int groupOrdinal, AsyncExpandableListView asyncExpandableListView) {
+            super(v, groupOrdinal, asyncExpandableListView);
+            textView = (TextView) v.findViewById(R.id.title);
+            mProgressBar = (ProgressBar) v.findViewById(R.id.progressBar);
+            mProgressBar.getIndeterminateDrawable().setColorFilter(0xFFFFFFFF,
+                    android.graphics.PorterDuff.Mode.MULTIPLY);
+            ivExpansionIndicator = (ImageView) v.findViewById(R.id.ivExpansionIndicator);
+        }
+
+
+        public TextView getTextView() {
+            return textView;
+        }
+
+
+        @Override
+        public void onGroupStartExpending() {
+            mProgressBar.setVisibility(View.VISIBLE);
+            ivExpansionIndicator.setVisibility(View.INVISIBLE);
+        }
+
+        @Override
+        public void onGroupExpanded() {
+            mProgressBar.setVisibility(View.GONE);
+            ivExpansionIndicator.setVisibility(View.VISIBLE);
+            ivExpansionIndicator.setImageResource(R.mipmap.ic_arrow_up);
+        }
+
+        @Override
+        public void onGroupCollapsed() {
+            mProgressBar.setVisibility(View.GONE);
+            ivExpansionIndicator.setVisibility(View.VISIBLE);
+            ivExpansionIndicator.setImageResource(R.mipmap.ic_arrow_down);
+
+        }
+    }
+    public static class NewsItemHolder extends RecyclerView.ViewHolder {
+
+
+        private final TextView tvTitle;
+        private final TextView tvDescription;
+
+        public NewsItemHolder(View v) {
+            super(v);
+            // Define click listener for the ViewHolder's View.
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                }
+            });
+            tvTitle = (TextView) v.findViewById(R.id.title);
+            tvDescription = (TextView) v.findViewById(R.id.description);
+        }
+
+        public TextView getTextViewTitle() {
+            return tvTitle;
+        }
+
+        public TextView getTextViewDescrption() {
+            return tvDescription;
+        }
+    }
+}
